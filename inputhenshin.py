7259d1c0ba6d0c8fe6b993cc1aca619bfd85a04d
Elijah Ferrer <fer2896@swtc.edu>
2026-08-25 12:07:43 -0500

    modified

--- inputhenshin.py
+++ inputhenshin.py
@@ -20,7 +20,7 @@ print("FULL-RABBIT MOONSAULT, RABBIT/TANK.")
 
 
 #*Reflections:
-#it is an input loop program that has an exit
+#it is an input loop program that has an exit. it only stops if i type in "HENSHIN" into the terminal which then it stops. 
 
 
 
