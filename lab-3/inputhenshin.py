01998d162feae022c53ff9e5196180b92fc3370e
Elijah Ferrer <fer2896@swtc.edu>
2026-08-25 12:16:22 -0500

    modified

---
+++ lab-3/inputhenshin.py
@@ -0,0 +1,26 @@
+""" 
+WEEK 2 DEMO 3 "HENSHIN!!"
+
+Purpose:
+to stop a loop with the right input
+
+Developer's note:
+going for another Kamen Rider themed code here
+"""
+
+command = ""
+
+#if i put anything that isn't "HENSHIN" it won't stop until i put in the right input
+
+while command !="HENSHIN":
+    command = input("WHAT YOU'RE TRANSFORMATION CALLOUT?: ")
+    print("ARE YOU READY?:", command)
+    
+print("FULL-RABBIT MOONSAULT, RABBIT/TANK.")
+
+
+#*Reflections:
+#it is an input loop program that has an exit. it only stops if i type in "HENSHIN" into the terminal which then it stops. 
+
+
+
